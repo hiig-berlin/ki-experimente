c5e4070b9bd4d8f1cb4bf636db8dc8cb74d34bd2
Esther Weidauer <esther@selfawaresoup.com>
2023-07-17 00:11:32 +0200

    custom face highlight, exp A

--- content/static/experiment-a.js
+++ content/static/experiment-a.js
@@ -12,11 +12,27 @@ const printDebug = data => {
 	//const detectorOptions = new faceapi.TinyFaceDetectorOptions()
 	const detectorOptions = new faceapi.SsdMobilenetv1Options()
 
+const drawDetections = (cvs, detections) => {
+	const ctx = cvs.getContext("2d")
+	ctx.lineWidth = 4
+	ctx.font = "16px Arial"
+	detections.forEach(d => {
+	const q = d._score * 2 - 1
+	ctx.strokeStyle = `hsl(${110*q},100%,50%)`
+	ctx.fillStyle = `hsl(${110*q},100%,50%)`
+		const box = d._box
+		if (!box) { return }
+		ctx.strokeRect(box._x, box._y, box._width, box._height)
+		ctx.fillText( `${Math.floor(d._score * 100)}%`, box._x + 6, box._y + 20)
+	})
+}
+
 const frameHandler = (vid, cvs) => {
 		return models.then(() => {
 			return faceapi.detectAllFaces(vid, detectorOptions)
 		}).then(faceDescriptions => {
-			faceapi.draw.drawDetections(cvs, faceDescriptions)
+			//faceapi.draw.drawDetections(cvs, faceDescriptions)
+			drawDetections(cvs, faceDescriptions)
 			printDebug(faceDescriptions)
 		})
 }
